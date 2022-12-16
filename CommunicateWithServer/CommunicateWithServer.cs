c649582b05649c1a737d94afb1508934a2007d0e
Andy Ponce <andy.ponce@outlook.com>
2022-12-16 08:36:51 -0500

    Added ability to communicate with local server in custom action

--- CommunicateWithServer/CommunicateWithServer.cs
+++ CommunicateWithServer/CommunicateWithServer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Net.Sockets;
+using System.Net;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
@@ -60,16 +62,75 @@ namespace CommunicateWithServer
         [JsonProperty("resultProperty")]
         public StringExpression ResultProperty { get; set; }
 
+        static int port = 11000;
+        static IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+        IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
         public override Task<DialogTurnResult> BeginDialogAsync(DialogContext dc, object options = null, CancellationToken cancellationToken = default(CancellationToken))
         {
             var arg1 = Arg1.GetValue(dc.State);
             var arg2 = Arg2.GetValue(dc.State);
-
-            var result = Convert.ToInt32(arg1) * Convert.ToInt32(arg2);
-            if (this.ResultProperty != null)
+            byte[] bytes = new byte[1024];
+            string result = null;
+            try
             {
-                dc.State.SetValue(this.ResultProperty.GetValue(dc.State), result);
+                // Connect to a Remote server
+                // Get Host IP Address that is used to establish a connection
+                // In this case, we get one IP address of localhost that is IP : 127.0.0.1
+                // If a host has multiple addresses, you will get a list of addresses
+
+                // Create a TCP/IP  socket.
+                Socket sender = new Socket(ipAddress.AddressFamily,
+                    SocketType.Stream, ProtocolType.Tcp);
+
+                // Connect the socket to the remote endpoint. Catch any errors.
+                try
+                {
+                    // Connect to Remote EndPoint
+                    sender.Connect(remoteEP);
+
+                    Console.WriteLine("Socket connected to {0}",
+                        sender.RemoteEndPoint.ToString());
+
+                    // Encode the data string into a byte array.
+                    byte[] msg = Encoding.ASCII.GetBytes("This is a test<EOF>");
+
+                    // Send the data through the socket.
+                    int bytesSent = sender.Send(msg);
+
+                    // Receive the response from the remote device.
+                    int bytesRec = sender.Receive(bytes);
+                    //Console.WriteLine("Echoed test = {0}",
+                    //    Encoding.ASCII.GetString(bytes, 0, bytesRec));
+                    result = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                    dc.State.SetValue(this.ResultProperty.GetValue(dc.State), result);
+                    // Release the socket.
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+
             }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            //var result = Convert.ToInt32(arg1) * Convert.ToInt32(arg2);
+            //if (this.ResultProperty != null)
+            //{
+            //    dc.State.SetValue(this.ResultProperty.GetValue(dc.State), result);
+            //}
 
             return dc.EndDialogAsync(result: result, cancellationToken: cancellationToken);
         }
